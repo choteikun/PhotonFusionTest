002af7dd78f1c8576d2c8e1e103572b680068b55
yuan0717zero <4a9k0017@stust.edu.tw>
2023-04-25 22:32:36 +0800

    修正消失

--- Assets/SHADER/ccTest.cs
+++ Assets/SHADER/ccTest.cs
@@ -7,7 +7,9 @@ public class ccTest : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
+    [SerializeField]
     private float playerSpeed = 2.0f;
+    [SerializeField]
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
