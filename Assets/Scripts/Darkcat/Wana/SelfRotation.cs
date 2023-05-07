28efa8d721221496a3577f0f49431d43e7ae870e
草蛇 <blackzxz77@gmail.com>
2023-05-07 22:10:40 +0800

    test

--- Assets/Scripts/Darkcat/Wana/SelfRotation.cs
+++ Assets/Scripts/Darkcat/Wana/SelfRotation.cs
@@ -8,6 +8,6 @@ public class SelfRotation : NetworkBehaviour
     [SerializeField] private float rotateSpeed_;
     public override void FixedUpdateNetwork()
     {
-        this.transform.Rotate(new Vector3(0, 1f, 0) * Runner.DeltaTime*rotateSpeed_);
+        this.transform.Rotate(new Vector3(0, 1f, 0) * Runner.DeltaTime * rotateSpeed_);
     }
 }
