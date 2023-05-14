fbbb326432f407d55bfddb46f72bef63846278c5
darkcatneko <darkcat7126@gmail.com>
2023-05-14 23:30:15 +0900

    SE Test

--- Assets/Scripts/Darkcat/Wana/SoundEffectTester.cs
+++ Assets/Scripts/Darkcat/Wana/SoundEffectTester.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class SoundEffectTester : MonoBehaviour
 {
     [SerializeField] private AudioSource AudioSourcePrefab;
+    [SerializeField] private AudioSource AudioSourceGlobal;
     [SerializeField] private AudioClip TestClip;
 
     public void PlayAudioTest()
     {
         AudioSourcePrefab.PlayOneShot(TestClip);
+    }
+    public void PlayAudioGlobalTest()
+    {
+        AudioSourceGlobal.PlayOneShot(TestClip);
     }
 }
