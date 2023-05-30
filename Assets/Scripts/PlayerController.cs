b8fc435f911dc91d79b167f24b9851b1cece1379
草蛇 <blackzxz77@gmail.com>
2023-05-30 23:57:52 +0800

    調整力道，新增Winner Scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -637,7 +637,7 @@ public class PlayerController : NetworkBehaviour
                     playerController.soundEffectPlay_RPC();
                     //playerController.Network_CharacterControllerPrototype.Jump();
                     playerController.Network_CharacterControllerPrototype.Move(Vector3.zero);
-                    playerController.Network_CharacterControllerPrototype.Velocity += airborneVec * (PushForce / 2 + playerController.PlayerGameData.BreakPoint);//垂直推力計算
+                    playerController.Network_CharacterControllerPrototype.Velocity += airborneVec * (PushForce / 2 + playerController.PlayerGameData.BreakPoint / 2);//垂直推力計算
                     playerController.Network_CharacterControllerPrototype.Velocity += pushDir * (PushForce + playerController.PlayerGameData.BreakPoint);//水平推力計算
 
                     playerController.LocalHurt = playerController.transform.InverseTransformDirection((playerController.transform.position - new Vector3(transform.position.x, 0, transform.position.z)));
